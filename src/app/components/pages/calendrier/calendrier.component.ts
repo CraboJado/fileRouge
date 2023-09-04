a4a80eb5d90e7642ab8e19909d918fb793b124ec
G33k0 <samedig@gmail.coom>
2023-09-04 10:25:55 +0200

    feat: mise en place fullcalendar

--- src/app/components/pages/calendrier/calendrier.component.ts
+++ src/app/components/pages/calendrier/calendrier.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { CalendarOptions } from '@fullcalendar/core';
+import frLocale from '@fullcalendar/core/locales/fr';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import interactionPlugin from '@fullcalendar/interaction'; 
 
 @Component({
   selector: 'app-departements',
@@ -6,5 +10,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./calendrier.component.css']
 })
 export class CalendrierComponent {
-
+  calendarOptions: CalendarOptions = {
+    plugins: [interactionPlugin, dayGridPlugin],
+    locale: frLocale,
+    initialView: 'dayGridMonth',
+    weekends: false,
+    selectable: true,
+    dateClick: function(info) {
+      alert('Clicked on: ' + info.dateStr);
+      alert('Coordinates: ' + info.jsEvent.pageX + ',' + info.jsEvent.pageY);
+      alert('Current view: ' + info.view.type);
+      // change the day's background color just for fun
+      info.dayEl.style.backgroundColor = 'red';
+    },
+    events: [
+      { title: 'Meeting', start: new Date() }
+    ]
+  };
 }
