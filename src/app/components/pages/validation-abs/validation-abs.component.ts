24b0a1adc46ef5c920ae848adb8619309602bb9f
kamella34 <vergnaud.kamella@bbox.fr>
2023-09-05 11:14:17 +0200

    test

--- src/app/components/pages/validation-abs/validation-abs.component.ts
+++ src/app/components/pages/validation-abs/validation-abs.component.ts
@@ -1,10 +1,50 @@
+import { JsonPipe } from '@angular/common';
 import { Component } from '@angular/core';
-
+import { Absence } from 'src/app/shared/model/absence';
+import { AbsenceService } from 'src/app/shared/service/absence.service';
 @Component({
   selector: 'app-validation-abs',
   templateUrl: './validation-abs.component.html',
   styleUrls: ['./validation-abs.component.css']
 })
 export class ValidationAbsComponent {
+  absences: Absence[] = [];
+  // isActive: boolean = false;
 
+
+
+  constructor(private _absenceService: AbsenceService) { }
+
+  ngOnInit(): void {
+    this._init()
+  }
+
+  private _init() {
+    this._absenceService
+      .findAll()
+      .subscribe(absenceReceived => {
+        this.absences = absenceReceived;
+        
+      })
+      
+  }
+
+
+  validStatut(absence: Absence) {
+    absence.statut = "VALIDEE";
+    this._absenceService
+      .update(absence)
+      .subscribe(() => this._init)
+  }
+
+
+  rejectStatut(absence: Absence) {
+    absence.statut = "REJETEE"
+  
+    this._absenceService
+      .update(absence)
+      .subscribe(() => this._init)
+  }
+  
 }
+
