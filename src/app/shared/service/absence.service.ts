8be6621497315d8603fd9933f9c4a7c139bdfc5f
kamella34 <vergnaud.kamella@bbox.fr>
2023-09-05 10:55:06 +0200

    message

---
+++ src/app/shared/service/absence.service.ts
@@ -0,0 +1,26 @@
+import {Injectable} from "@angular/core";
+import {HttpClient} from "@angular/common/http";
+import {Login} from "../model/login";
+import {Departement} from "../model/departement";
+import {Absence} from "../model/absence";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AbsenceService{
+
+  private _baseUrl = "http://localhost:8080/absence";
+
+
+  constructor(private _http: HttpClient) {}
+
+
+  public findAll(){
+    return this._http.get<Absence[]>(this._baseUrl)
+  }
+
+  public update(updated: Absence) {
+    return  this._http
+      .put(`${this._baseUrl}/statut/${updated.id}`, updated) 
+  }
+}
