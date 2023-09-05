a99299c5797276622ed3d1e1516fecc33eeccc62
Antoine L <antoine.ligerot@outlook.fr>
2023-09-05 17:31:12 +0200

    histogramme fonctionne

--- src/app/components/pages/rapports-vue-one/rapports-vue-one.component.ts
+++ src/app/components/pages/rapports-vue-one/rapports-vue-one.component.ts
@@ -1,12 +1,15 @@
 import {Component, OnInit} from '@angular/core';
 import {AbsenceService} from "../../../shared/service/absence.service";
 import {Absence} from "../../../shared/model/absence";
-import {ChartOptions} from "chart.js";
+import {ChartConfiguration, ChartOptions} from "chart.js";
 import {formatDate} from "@angular/common";
 import {DepartementService} from "../../../shared/service/departement.service";
 import {EmployeService} from "../../../shared/service/employe.service";
 import {Departement} from "../../../shared/model/departement";
 import {Employe} from "../../../shared/model/employe";
+import {switchMap} from "rxjs";
+import {JoursOffService} from "../../../shared/service/joursOff.service";
+import {JoursOff} from "../../../shared/model/joursOff";
 
 @Component({
   selector: 'app-rapports-vue-one',
@@ -20,88 +23,128 @@ export class RapportsVueOneComponent implements  OnInit{
   employes:Employe[]=[]
   departements:Departement[]=[]
 
+  joursOffs:JoursOff[]=[]
+
+  departementGeneral:Departement={id:0,name:"tout le monde"}
+
   currentMonth:number = new Date().getMonth();
 
   currentYear:number=new Date().getFullYear()
 
 
-  absenceList:Date[] = [
-    new Date('2013-04-13'),
-    new Date('2013-04-14'),
-    new Date('2013-04-13')
-  ];
 
- // lineChartData: any[] = [{ data: Array(50).fill(1), label: 'bonjour' }];
-  lineChartData: any[] = [ {
-    data: [], // Your data values for the first dataset
-    label: 'Dataset 1',
-    backgroundColor: 'rgba(255, 99, 132, 0.2)', // Color for the bars
-    stack : 'stack 1'
-  },
-    {
-      data: [], // Your data values for the second dataset
-      label: 'Dataset 2',
-      backgroundColor: 'rgba(54, 162, 235, 0.2)', // Color for the bars
-      stack: 'stack 1'
-    },];
+
+  lineChartData: any[]=[]
   lineChartLabels: string[] = []
   constructor(private absenceService:AbsenceService,
               private departementService:DepartementService,
-              private employeService:EmployeService) {
+              private employeService:EmployeService,
+              private jourOffService:JoursOffService) {
   }
+  employeByDepartement(departementId:string){
 
-
-  employeByDepartement(departementId:number){
-
-    this.employeService.findAll().subscribe(t=>{
-      this.employes=t;
-      this.employes.filter(t=>t.departement?.id==departementId);
+    if(parseInt(departementId)==0) {
+      this.employeService.findAll().subscribe(list=>{
+        this.employes=list
+        this.updateLineChartLabels()
+      })
+    }else{
+    this.employeService.findAll().subscribe(list=>{
+      this.employes=list.filter(employe=>employe.departement?.id==parseInt(departementId));
+      this.updateLineChartLabels()
     })
-
-
-
-  }
-
-
-
-nbAbsencePerDay(date:string):number{
-  let nbTotal=0;
-  for(let absence of this.absences){
-      if(absence.dateDebut?.toString()==date.toString()){
-        nbTotal++;
     }
-}
-  return nbTotal
-}
 
-nbTest(date:string):number{
-  let nbTotal=0;
-  for(let absence of this.absences){
-    if(absence.dateDebut?.toString()!=date.toString()){
-      nbTotal++;
+  }
+
+
+  updateLineChartData(){
+    this.lineChartData = this.employes.map((employe, index) => {
+      const dataPoints = this.lineChartLabels.map((label) =>
+        this.nbAbsencePerDayPerEmploye(label, employe)
+      );
+      const hue=(index / this.employes.length) * 360;
+      return {
+        data: dataPoints, // Array of data points for the dataset
+        label: employe.firstName,
+        backgroundColor: `hsla(${hue}, 100%, 50%, 1)`,
+        stack: 'stack 1',
+      };
+    });
+  }
+
+
+
+  nbAbsencePerDayPerEmploye(date:string,employe:Employe){
+    let nbTotal=0;
+
+
+    for(let absence of this.absences) {
+      for(let dateTempo of this.getDates(absence.dateDebut, absence.dateFin)){
+        if (date == dateTempo && absence.employe?.id == employe.id) {
+          nbTotal++;
+        }
+      }
+
     }
+    return nbTotal
   }
-  return nbTotal
-
-}
 
 
-  lineChartInit(){
-      this.lineChartData[0].data = this.lineChartLabels.map(label => this.nbAbsencePerDay(label));
-      this.lineChartData[1].data= this.lineChartLabels.map(label => this.nbTest(label));
+
+   getDates(startDate: Date, endDate: Date) {
+    const dates: string[] = [];
+    let currentDate = new Date(startDate);
+    let maxDate= new Date(endDate)
+    while (currentDate<=new Date(endDate)) {
+      for(let jours of this.joursOffs){
+
+        if(jours.jour==currentDate) {
+          currentDate.setDate(currentDate.getDate() + 1);
+        }
+      }
+      dates.push(formatDate(currentDate, 'yyyy-MM-dd', 'en-US'));
+      currentDate.setDate(currentDate.getDate() + 1);
+      }
+    return dates;
   }
-  lineChartOptions: any = {
-    responsive: true,
+
+
+
+
+
+  lineChartOptions: ChartConfiguration['options'] = {
+    responsive:true,
+    scales: {
+      // We use this empty structure as a placeholder for dynamic theming.
+      y: {
+        position: 'left',
+        suggestedMax:5,
+      },
+    },
   };
   lineChartLegend = true;
   lineChartType = 'line';
 
   ngOnInit(): void {
-    this.absenceService.findAll().subscribe(t => {
-      this.absences = t;
-      this.updateLineChartLabels()
+    this.employeService.findAll().pipe(
+      switchMap((employes) => {
+        this.employes = employes;
+        return this.absenceService.findAll();
+      }),
+      switchMap((absences) => {
+        this.absences = absences as Absence[];
+        return this.jourOffService.findAll();
+      })
+    ).subscribe((joursOff) => {
+      this.joursOffs = joursOff;
+      this.updateLineChartLabels();
     });
 
+    this.departementService.findAll().subscribe(t=>{
+      this.departements=t
+      this.departements.push(this.departementGeneral)
+    })
 
   }
 
@@ -119,7 +162,7 @@ nbTest(date:string):number{
       this.currentYear+=-1
     }
 
-    this.updateLineChartLabels();
+   this.updateLineChartLabels()
   }
   updateLineChartLabels() {
     this.lineChartLabels = Array.from({ length: 31 }, (_, i) => {
@@ -128,9 +171,13 @@ nbTest(date:string):number{
       date.setDate(i + 1);
       date.setFullYear(this.currentYear)
       return formatDate(date, 'yyyy-MM-dd', 'en-US');
+
+
     });
-    this.lineChartInit();
+
+      this.updateLineChartData()
   }
+
 
 
 
