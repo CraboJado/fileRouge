ebaffc99ed182dfe7d8d19b7422f7350c6cad8f7
kamella34 <vergnaud.kamella@bbox.fr>
2023-09-05 11:20:57 +0200

    model

---
+++ src/app/shared/model/absence.ts
@@ -0,0 +1,13 @@
+import {Employe} from "./employe";
+
+export interface Absence{
+  id?:number;
+  dateCreation?:Date;
+  dateDebut?:Date;
+  dateFin?:Date;
+  motif?:string;
+  typeAbsence?:string;
+  statut?:string;
+  employe?:Employe;
+
+}
