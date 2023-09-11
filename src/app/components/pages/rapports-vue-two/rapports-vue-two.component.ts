1b3c1314ea0ea93a1040a58319b8c6e67e369dd8
kamella34 <vergnaud.kamella@bbox.fr>
2023-09-11 12:19:48 +0200

    vueTwoo

--- src/app/components/pages/rapports-vue-two/rapports-vue-two.component.ts
+++ src/app/components/pages/rapports-vue-two/rapports-vue-two.component.ts
@@ -28,7 +28,8 @@ export class RapportsVueTwoComponent implements OnInit {
   joursOffs: JoursOff[] = []
   departementGeneral: Departement = { id: 0, name: "tout le monde" }
   currentMonth: number = new Date().getMonth();
-  currentYear: number = new Date().getFullYear()
+  currentYear: number = new Date().getFullYear();
+  currentYear2: number = new Date().getFullYear()
   // daysInMonth: number = 0; // Initialisez à 0
 
   lineChartData: any[] = []
@@ -163,6 +164,7 @@ export class RapportsVueTwoComponent implements OnInit {
     for (let i = 1980; i < 2050; i++) {
       this.annees.push(i)
     }
+    
 
   }
 
